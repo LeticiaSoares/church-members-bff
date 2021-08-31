6a3cbc32f6a6f91c59be7f19229b086e0975a678
LeticiaSoares <lele_leticia.94@hotmail.com>
2021-08-31 15:42:28 -0300

    feat: connected with aws

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const PORT = process.env.PORT || 3000
-
-app.get("/ping",()=>(
-    res.send('pong')
-))
-
-app.listen(PORT,()=>(
-    console.log('Listening on port ' + PORT)
-))
-
-
-
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+//GET
+app.get('/', (req, res) => {
+    res.status(200).send('hello world!');
+});
+//GET
+//Simply sends the current time
+app.get('/time', (req, res) => {
+    let timeNow = Date(Date.now());
+    res.status(200).send(timeNow.toString());
+});
+module.exports = app;
