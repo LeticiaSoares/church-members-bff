2c4309d77aa4f01c73d22b4c56f01a7c4799232f
LeticiaSoares <lele_leticia.94@hotmail.com>
2021-09-30 18:49:28 -0300

    feat: add whitelist of cors

--- index.js
+++ index.js
@@ -13,27 +13,26 @@ require('dotenv').config()
 
 const app = express()
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", process.env.CHURCH_ADMIN || "http://localhost:8080");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cookie");
-    res.header("Access-Control-Allow-Credentials", true);
-    next()
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", process.env.CHURCH_ADMIN || "http://localhost:8080");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cookie");
+//
+//     next()
+// });
 
-const allowedOrigins = [
-    'http://localhost:8080',
-    'http://church-members-admin.s3-website-us-east-1.amazonaws.com/production'];
-
-app.use(cors({
-    origin: function(origin, callback){
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
+app.use((req, res, next) => {
+    const corsWhitelist = [
+        'http://localhost:8080',
+        'http://church-members-admin.s3-website-us-east-1.amazonaws.com',
+    ];
+    if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Cookie');
+        res.header("Access-Control-Allow-Credentials", true);
     }
-}));
+
+    next();
+});
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
