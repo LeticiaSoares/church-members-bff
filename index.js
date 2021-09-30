cad2349284dd5c149c0afe30253a514bfdc2e2cb
LeticiaSoares <lele_leticia.94@hotmail.com>
2021-09-30 18:13:04 -0300

    feat: add cors in headers

--- index.js
+++ index.js
@@ -14,25 +14,26 @@ require('dotenv').config()
 const app = express()
 
 app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", process.env.CHURCH_ADMIN || "http://localhost:8080");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cookie");
     res.header("Access-Control-Allow-Credentials", true);
     next()
 });
 
-const allowedOrigins = [
-    'http://localhost:8080',
-    'http://church-members-admin.s3-website-us-east-1.amazonaws.com/production'];
-
-app.use(cors({
-    origin: function(origin, callback){
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}));
+// const allowedOrigins = [
+//     'http://localhost:8080',
+//     'http://church-members-admin.s3-website-us-east-1.amazonaws.com/production'];
+//
+// app.use(cors({
+//     origin: function(origin, callback){
+//         if(!origin) return callback(null, true);
+//         if(allowedOrigins.indexOf(origin) === -1){
+//             const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+//             return callback(new Error(msg), false);
+//         }
+//         return callback(null, true);
+//     }
+// }));
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
